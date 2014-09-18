9d7f0c7df0bc3b6f840ceeee7b56f4fb7fcec404
qiang2010 <qiangji1991@gmail.com>
2014-09-18 21:49:45 +0800

    c

---
+++ codeForTesting/iostreamTesting.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+  cout<< "Please input two num:"<<endl;
+  double n1,n2;
+  cin>>n1>>n2;
+
+  // endl 换行和刷新缓冲区功能
+  cout<<"sum:"<<n1+n2<<endl;
+
+  return 0;
+
+}
