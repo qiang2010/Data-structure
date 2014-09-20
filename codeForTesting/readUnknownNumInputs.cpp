2abab630e0e17bb2712031e3c878587bd4f7a2f6
qiang <qiangji1991@gmail.com>
2014-09-20 17:55:58 +0800

    data stru

---
+++ codeForTesting/readUnknownNumInputs.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int sum=0;
+  int input;
+  while (cin>>input)
+    sum += input;
+  cout << sum;
+
+
+}
---
+++ codeForTesting/test1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+   int m = 011, h = 07;
+   cout << m << "  "<<h<<endl;
+
+   int m2 = 9 ,h2 = 7;
+   cout << m2 <<"  "<< h2 <<endl
+
+   double a=b = 0.9;
+}
